90c436fe0a9a4293425dbf0ed322559e615d7899
Artem <inste@icloud.com>
2019-05-16 13:08:52 +0300

    third commit

--- LabelVFLlayout/ViewController.swift
+++ LabelVFLlayout/ViewController.swift
@@ -23,10 +23,17 @@ class ViewController: UIViewController, LabelProtocol {
         
         let dictionary = ["l0":myLabels[0],"l1":myLabels[1],"l2":myLabels[2],"l3":myLabels[3],"l4":myLabels[4]]
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-50-[l0]-10-[l1]-10-[l2]-10-[l3]-10-[l4]", options: [], metrics: nil, views: dictionary))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-50-[l0]-10-[l1]-10-[l2]-10-[l3]-10-[l4]", options: [], metrics: nil, views: dictionary))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[l0]-10-[l1]-10-[l2]-10-[l3]-10-[l4]", options: [], metrics: nil, views: dictionary))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[l0]-10-[l1]-10-[l2]-10-[l3]-10-[l4]", options: [], metrics: nil, views: dictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[l0]", options: [], metrics: nil, views: dictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[l1]", options: [], metrics: nil, views: dictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[l2]", options: [], metrics: nil, views: dictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[l3]", options: [], metrics: nil, views: dictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[l4]", options: [], metrics: nil, views: dictionary))
     }
     
     
 }
+
 
